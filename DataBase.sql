04ba88aa092bae7684f0f95ab0bdba5e83d28dc7
sergilor <81633427+sergilor@users.noreply.github.com>
2021-03-30 16:41:39 +0200

    Create DataBase.sql

---
+++ DataBase.sql
@@ -0,0 +1,15 @@
+Create table users (
+
+NICKNAME varchar(100) UNIQUE,
+PSSWORD varchar(100)
+
+);
+
+Create table conversation(
+
+NICKNAME1 varchar(100) ,
+NICKNAME2 varchar(100),
+MESSAGE varchar(1000),
+MOMENT DATE
+
+);
