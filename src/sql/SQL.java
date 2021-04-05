c4e1126f6f44d0ed976569bbf544a315251c8141
petio01bg <81573779+petio01bg@users.noreply.github.com>
2021-04-05 18:07:20 +0200

    Update SQL.java
    Updated sql file

--- src/sql/SQL.java
+++ src/sql/SQL.java
@@ -2,34 +2,62 @@ package sql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class SQL {
+
+public class SQL{
 	
-	private Connection connect() {
-        String url = "jdbc:oracle:thin:@localhost:1521:xe"; // Aquí no se que poner.
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+
+    public void insert(Connection conn, String NICKNAME, int PSSWORD) throws ClassNotFoundException, SQLException {
+      
+
+        Statement stmt = conn.createStatement();
+        stmt.executeQuery("INSERT INTO users (NICKNAME,PSSWORD) Values ('" + NICKNAME + "','" + PSSWORD +"')");
+        
+        System.out.println("Insertadas dos personas\n");
+        conn.commit();
+    }
+    
+    public void uso(String user, int ps) {
+    	 try {
+    	        
+             Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "petio01bg", "**");
+
+             if (conn != null) {
+                 conn.setAutoCommit(false); // Desactivamos el autoCommit
+                 
+                 insert(conn,user,ps);
+                 todasPersonas(conn);
+             
+                 conn.close();  
+             } else {
+                 System.out.println("Failed to make connection!");
+             }
+
+         } catch (SQLException e) {
+             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
+    }
+    
+    public static void todasPersonas(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT NICKNAME, PSSWORD FROM users");
+        System.out.println("Listado de personas");
+        int cont = 1;
+        while(rs.next()) {
+            System.out.println(cont + ") " + rs.getString("NICKNAME") + "\t" + rs.getString("PSSWORD"));
+            cont++;
         }
-        return conn;
+        System.out.println();
     }
 
-    public void insert(String NICKNAME, int PSSWORD) {
-        String sql = "INSERT INTO USERS(NICKNAME,PSSWORD) VALUES(?,?)";
+    
 
-        try (Connection conn = this.connect();
-                PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, NICKNAME);
-            pstmt.setInt(2, PSSWORD);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
+}
 
     
 
