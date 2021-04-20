e9a6ebe39b1da39c718e66f3ad44bbc9765ff266
sergilor <81633427+sergilor@users.noreply.github.com>
2021-04-20 11:50:46 +0200

    Update InsertDB.java

--- src/SQL/InsertDB.java
+++ src/SQL/InsertDB.java
@@ -6,8 +6,12 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class InsertDB {
+	
+	private int _nextID = 0;
+	
+	
     private Connection connect() {
-        String url = "jdbc:oracle:thin:@localhost:1521:xe"; // Aquí no se que poner (dirección para conectarse a la base de datos)
+        String url = "jdbc:oracle:thin:@localhost:1521:xe"; // AquÃ­ no se que poner (direcciÃ³n para conectarse a la base de datos)
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url);
@@ -18,13 +22,29 @@ public class InsertDB {
     }
 
     public void insert(String NICKNAME, int PSSWORD) {
-        String sql = "INSERT INTO USERS(NICKNAME,PSSWORD) VALUES(?,?)";
+        String sql = "INSERT INTO USERS(NICKNAME,PSSWORD,_nextID) VALUES(?,?)";
 
         try (Connection conn = this.connect();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, NICKNAME);
             pstmt.setInt(2, PSSWORD);
             pstmt.executeUpdate();
+            _nextID++;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    //elimina una fila
+    public void delate(int DELETEID) {
+        String sql = "DELETE FROM USERS WHERE (ID = ?)";
+
+        try (Connection conn = this.connect();
+        		
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, DELETEID);
+            pstmt.executeUpdate();
+            
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
